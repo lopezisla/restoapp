1b1586b37d519b35656fdd2dd0b55243d1b3bf01
Jorge Alejandro López Isla <jorgelopezisla@gmail.com>
2020-04-29 19:53:07 -0300

    primer commit

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Button, Icon } from "react-native-elements";
+//import Icon from "react-native-vector-icons/FontAwesome";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Resto App</Text>
+      <Button
+        icon={<Icon type="material" name="check" size={15} color="white" />}
+        title=" Click"
+      />
     </View>
   );
 }
@@ -12,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
